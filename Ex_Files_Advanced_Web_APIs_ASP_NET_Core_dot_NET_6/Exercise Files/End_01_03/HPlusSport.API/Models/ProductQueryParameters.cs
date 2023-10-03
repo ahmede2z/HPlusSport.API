f4c97201468a1cb2ab445bb135020a2d67f86ac4
Ahmed Ezz <ahmede2zman@gmail.com>
2023-10-03 15:36:38 +0300

    Finsh course 2

--- Ex_Files_Advanced_Web_APIs_ASP_NET_Core_dot_NET_6/Exercise Files/End_01_03/HPlusSport.API/Models/ProductQueryParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HPlusSport.API.Models
-{
-    public class ProductQueryParameters : QueryParameters
-    {
-        public decimal? MinPrice { get; set; }
-        public decimal? MaxPrice { get; set; }
-    }
-}
--- Ex_Files_Building_Web_APIs_ASP_NET_Core_dot_NET_6/Ex_Files_Building_Web_APIs_ASP_NET_Core_dot_NET_6/Exercise Files/End_0305/HPlusSport/HPlusSport.API/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HPlusSport.API.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public virtual List<Product> Products { get; set; }
-    }
-}
