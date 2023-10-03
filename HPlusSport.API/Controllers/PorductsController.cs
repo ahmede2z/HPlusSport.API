61b478b68eafd0cb175393d075bd07365e7439f8
Ahmed Ezz <ahmede2zman@gmail.com>
2023-10-03 13:21:16 +0300

    Add API Versioning{URL V}

--- HPlusSport.API/Controllers/PorductsController.cs
+++ HPlusSport.API/Controllers/PorductsController.cs
@@ -5,13 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    //[Route("api/[controller]")]
+    [Route("v{V:apiVersion}/products")]
     [ApiController]
-    public class PorductsController : ControllerBase
+    public class PorductsV1Controller : ControllerBase
     {
         private readonly ShopContext _context;
 
-        public PorductsController(ShopContext context)
+        public PorductsV1Controller(ShopContext context)
         {
 
             _context = context;
@@ -22,6 +24,168 @@ namespace HPlusSport.API.Controllers
         public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
         {
             IQueryable<Product> products = _context.Products;
+
+            if (queryParameters.MinPrice != null)
+            {
+                products = products.Where(
+                    p => p.Price >= queryParameters.MinPrice.Value);
+            }
+
+            if (queryParameters.MaxPrice != null)
+            {
+                products = products.Where(
+                    p => p.Price <= queryParameters.MaxPrice.Value);
+            }
+
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(
+                    p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower()
+                             .Contains(queryParameters.Name.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.SearchTirm))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower()
+                             .Contains(queryParameters.Name.ToLower())
+                         || p.Sku.ToLower()
+                             .Contains(queryParameters.Name.ToLower())
+                    );
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.SortBy))
+            {
+                if (typeof(Product).GetProperty(queryParameters.SortBy) != null)
+                {
+                    products = products.OrderByCustom(
+                        queryParameters.SortBy,
+                        queryParameters.SortOrder);
+                }
+            }
+
+
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+
+            return Ok(await products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id }
+                , product);
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<IActionResult> DeleteMultipleProduct([FromQuery] int[] ids)
+        {
+            var products = new List<Product>();
+            foreach (int id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                products.Add(product);
+            }
+
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+
+
+    [ApiVersion("2.0")]
+    //[Route("api/[controller]")]
+    [Route("v{V:apiVersion}/products")]
+    [ApiController]
+    public class PorductsV2Controller : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public PorductsV2Controller(ShopContext context)
+        {
+
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
+        {
+            IQueryable<Product> products = _context.Products.Where(p => p.IsAvailable == true);
 
             if (queryParameters.MinPrice != null)
             {
--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -6,6 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(option =>
+    {
+        option.ReportApiVersions = true;
+        option.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+        option.AssumeDefaultVersionWhenUnspecified = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
